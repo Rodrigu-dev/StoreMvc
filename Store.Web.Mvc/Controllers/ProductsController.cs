c52a4024cfc5ed6391277da63a4e9d7d6a8509a5
Rodrigu-dev <rodrigu.dev@outlook.com>
2021-10-08 15:36:28 -0300

    Conclusão

---
+++ Store.Web.Mvc/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Application.Interfaces;
+using Store.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Mvc.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Index()
+        {
+            ProductViewModel model = _productService.GetProducts();
+            return View(model);
+        }
+    }
+}
